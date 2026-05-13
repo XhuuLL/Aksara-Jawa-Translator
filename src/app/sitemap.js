5385eda041d218e3864b5bb2f0e9f231065eb7ae
Fatkhul <fatkhularif15@gmail.com>
2026-05-13 21:57:34 +0700

    update shearch google

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://www.aksarajawa-translate.my.id',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ];
+}
